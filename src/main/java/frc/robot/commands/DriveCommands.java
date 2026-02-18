2b4b40d0ce24f5cbf16ec36df8b0165df0594ade
Garrett Hart <garrett7621@gmail.com>
2026-02-17 22:08:19 -0500

    linting

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -36,7 +36,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
-
 import org.littletonrobotics.junction.Logger;
 
 public class DriveCommands {
